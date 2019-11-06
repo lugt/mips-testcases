86634ccc74c7c97aad4f118ffa053e3afb0002a2
Lgt_w10 <lu.gt@163.com>
2019-11-06 09:59:34 +0800

    Finished new multiply

--- real/dest.s
+++ real/dest.s
@@ -1,3 +1,12 @@
+.data
+mes:  .asciiz "Hello Computer Systems (III) 2017152003!\n"
+
+CONTROL: .word32 0x10000
+DATA:    .word32 0x10008
+
+PDATR:   .word32 0xBEADFEED
+QDATR:   .word32 0xFEADFADE
+
 	.text
 multime:
 	daddui	r29,r29,-32
@@ -14,7 +23,7 @@ multime:
 	nop
 	sw	r2,20(r30)
 	sw	r0,8(r30)
-	b	$L2
+	j	$L2
 	nop
 
 $L4:
@@ -37,7 +46,7 @@ $L3:
 $L2:
 	lw	r2,8(r30)
 	nop
-	slt	r2,r2,32
+	slti	r2,r2,32
 	bne	r2,r0,$L4
 	nop
 
@@ -45,6 +54,15 @@ $L2:
 	dadd	r29,r30,r0
 	lw	r30,28(r29)
 	daddui	r29,r29,32
+
+	lwu r8,DATA(r0)    ; get data
+	lwu r9,CONTROL(r0) ; and control registers
+
+	daddi r16,r0,1       ; set for ascii output
+	dadd r17,r0,r2
+	sd r17,0(r8)           ; write address of message to DATA register
+	sd r16,0(r9)           ; make it happen
+
 	halt
 	nop
 
